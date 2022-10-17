58e26235552fb5247fd27efebc8d296b8d02a410
MouslimOSV <m.oussoupkhadjiev@thomas-piron.eu>
2022-10-17 13:09:33 +0200

    Update WeatherForecast.cs

--- TP_FormationGit/WeatherForecast.cs
+++ TP_FormationGit/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace TP_FormationGit
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
 
         public string Demo { get; set; }
 
